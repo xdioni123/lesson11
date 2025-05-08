247ee4f1bf082dc6f26ff54a68c728e47afd00c5
xdioni123 <dion.ibrahimi2008@gmail.com>
2025-05-08 20:16:38 +0200

    lesson11

---
+++ main.py
@@ -0,0 +1,10 @@
+file_path = "C:/Users/Student/lesson11/leximi.txt"
+# file = open(file_path, "r")
+
+# content = file.read()
+# print(content)
+
+with open(file_path, "r") as file:
+    content = file.read()
+    print(content)
+
