62fca74cb8b9a91f2c301ecc79ca22be695fdb24
xdioni123 <dion.ibrahimi2008@gmail.com>
2025-05-08 20:48:45 +0200

    lesson11

--- main.py
+++ main.py
@@ -4,7 +4,18 @@ file_path = "C:/Users/Student/lesson11/leximi.txt"
 # content = file.read()
 # print(content)
 
-with open(file_path, "r") as file:
-    content = file.read()
-    print(content)
+# with open(file_path, "r") as file:
+#     content = file.read()
+#     print(content)
 
+# with open(file_path, "r") as file:
+#     line1 = file.readline()
+#     print(line1)
+
+# with open(file_path, "w") as file:
+#     file.write("Hello World")
+
+# with open(file_path, "r") as file:
+#     file.seek(20)
+#     data = file.read()
+#     print(data)
